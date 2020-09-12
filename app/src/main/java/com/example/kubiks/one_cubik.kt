6e1ce7388d35a72767b2bc9fa0c16f03928b1253
mb91 <mb87ber111@yandex.ru>
2020-09-12 15:18:11 +0300

    update Mark 01 09

--- app/src/main/java/com/example/kubiks/one_cubik.kt
+++ app/src/main/java/com/example/kubiks/one_cubik.kt
@@ -2,18 +2,35 @@ package com.example.kubiks
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_one_cubik.*
 import kotlin.random.Random
 
 class one_cubik : AppCompatActivity() {
-
+    var was = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_cubik)
-    }
+        val button_id = findViewById<ImageButton>(R.id.pbutton) as ImageButton
+        if (was == false) {
+            button_id.setOnTouchListener(object : View.OnTouchListener {
+                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                    when (event?.action){
+                        MotionEvent.ACTION_DOWN -> animation1_1()//Do Something
+                        MotionEvent.ACTION_UP -> animation1_2()
+                    }
+
+                    return v?.onTouchEvent(event) ?: true
+                }
+            })
+        }
+        }
 
     fun go_back(view: View){
         val back_activity = Intent(this, MainActivity::class.java)
@@ -52,4 +69,47 @@ class one_cubik : AppCompatActivity() {
             6 -> change_img_six(view)
         }
     }
+    fun animation1_1() {
+            was = true
+            val imageView = findViewById(R.id.pbutton) as ImageView
+            imageView.setImageDrawable(null)
+            imageView.setBackgroundResource(R.drawable.anim)
+            val animationDrawable = imageView.background as AnimationDrawable
+            if (!animationDrawable.isRunning) {
+
+                var totalFrameDuration = 0
+                for (i in 0 until animationDrawable.numberOfFrames) {
+                    totalFrameDuration += animationDrawable.getDuration(i)
+                }
+
+                animationDrawable.start()
+                Handler().postDelayed(
+                    Runnable { animationDrawable.stop() },
+                    totalFrameDuration.toLong()
+                )
+            }
+            was = false
+        }
+    fun animation1_2() {
+            was = true
+            val imageView = findViewById(R.id.pbutton) as ImageView
+            imageView.setImageDrawable(null)
+            imageView.setBackgroundResource(R.drawable.anim2)
+            val animationDrawable = imageView.background as AnimationDrawable
+            if (!animationDrawable.isRunning) {
+
+                var totalFrameDuration = 0
+                for (i in 0 until animationDrawable.numberOfFrames) {
+                    totalFrameDuration += animationDrawable.getDuration(i)
+                }
+
+                animationDrawable.start()
+                Handler().postDelayed(
+                    Runnable { animationDrawable.stop() },
+                    totalFrameDuration.toLong()
+                )
+            }
+            was = false
+        }
+
 }
